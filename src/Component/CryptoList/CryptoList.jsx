73efccc8ff4a6e118f9a2d379a40a7b09c49a1c2
adityaexe <kmradi2002@gmail.com>
2024-08-26 23:14:45 +0530

    ok

--- src/Component/CryptoList/CryptoList.jsx
+++ src/Component/CryptoList/CryptoList.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./CryptoList.css"; // Ensure Tailwind CSS is imported here
 
 const CryptoList = () => {
