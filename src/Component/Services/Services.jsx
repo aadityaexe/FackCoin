1343417231a9160dff359cb8ae91f6252b642005
adityaexe <kmradi2002@gmail.com>
2025-01-02 08:57:58 +0530

    refactor Services component for improved readability and remove unnecessary animations

--- src/Component/Services/Services.jsx
+++ src/Component/Services/Services.jsx
@@ -1,6 +1,3 @@
-import { useEffect, useState } from "react";
-import "./Services.css"; // Import the CSS file for transitions
-
 const servicesData = [
   {
     title: "Crypto Trading",
@@ -28,51 +25,23 @@ const servicesData = [
 ];
 
 const Services = () => {
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    setIsVisible(true);
-  }, []);
-
   return (
-    <section className="bg-gradient-to-b z-0py-12">
+    <section className="py-12">
       <div className="container mx-auto px-4">
-        <h2
-          className={`text-4xl font-bold text-white text-center mb-8 transform transition-h2 ${
-            isVisible ? "visible-h2" : "hidden-h2"
-          }`}
-        >
+        <h2 className="text-3xl font-bold text-gray-900 text-center mb-8">
           Our Services
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {servicesData.map((service, index) => (
             <div
               key={index}
-              className={`services-card transition-card ${
-                isVisible ? "visible-card" : "hidden-card"
-              }`}
+              className="bg-gray-900 shadow-md rounded-lg p-6 text-center"
             >
-              <div
-                className={`text-4xl mb-4 transition-icon ${
-                  isVisible ? "visible-icon" : "hidden-icon"
-                }`}
-              >
-                {service.icon}
-              </div>
-              <h3
-                className={`text-2xl font-bold mb-2 text-gray-900 transition-h3 ${
-                  isVisible ? "visible-h3" : "hidden-h3"
-                }`}
-              >
+              <div className="text-4xl mb-4">{service.icon}</div>
+              <h3 className="text-xl font-bold mb-2 text-gray-800">
                 {service.title}
               </h3>
-              <p
-                className={`text-gray-700 transition-p ${
-                  isVisible ? "visible-p" : "hidden-p"
-                }`}
-              >
-                {service.description}
-              </p>
+              <p className="text-gray-600">{service.description}</p>
             </div>
           ))}
         </div>
