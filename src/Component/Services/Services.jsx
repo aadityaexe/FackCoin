d82575a4b8f151f8d3fd28e8416c89760b1ccca6
adityaexe <kmradi2002@gmail.com>
2025-01-02 09:23:13 +0530

    refactor Services component for improved styling and text readability

--- src/Component/Services/Services.jsx
+++ src/Component/Services/Services.jsx
@@ -28,20 +28,17 @@ const Services = () => {
   return (
     <section className="py-12">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-gray-900 text-center mb-8">
+        <h2 className="text-3xl font-bold text-purple-700 text-center mb-8">
           Our Services
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {servicesData.map((service, index) => (
-            <div
-              key={index}
-              className="bg-gray-900 shadow-md rounded-lg p-6 text-center"
-            >
-              <div className="text-4xl mb-4">{service.icon}</div>
-              <h3 className="text-xl font-bold mb-2 text-gray-800">
+            <div key={index} className=" shadow-md rounded-lg p-6 text-center">
+              <div className="text-4xl mb-4 ">{service.icon}</div>
+              <h3 className="text-xl font-bold mb-2 bg-[#cadad3] rounded-lg">
                 {service.title}
               </h3>
-              <p className="text-gray-600">{service.description}</p>
+              <p className=" text-black rounded-xl">{service.description}</p>
             </div>
           ))}
         </div>
