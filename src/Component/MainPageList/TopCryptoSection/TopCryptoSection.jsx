5b6212ed82b97773ffda9011d563b63a9650b390
adityaexe <kmradi2002@gmail.com>
2025-01-02 08:57:53 +0530

    refactor TopCryptoSection component for improved readability and styling

--- src/Component/MainPageList/TopCryptoSection/TopCryptoSection.jsx
+++ src/Component/MainPageList/TopCryptoSection/TopCryptoSection.jsx
@@ -1,35 +1,16 @@
-import { useEffect, useState } from "react";
-import "./TopCryptoSection.css";
 import { useNavigate } from "react-router-dom";
 
 const TopCryptoSection = () => {
-  const [isVisible, setIsVisible] = useState(false);
   const nav = useNavigate();
 
-  useEffect(() => {
-    setIsVisible(true);
-  }, []);
-
   return (
-    <div className="flex flex-col items-center z-0 justify-center h-[50vh] text-white p-8">
-      <h1
-        className={`text-4xl font-bold mb-4 transition-h1 ${
-          isVisible ? "visible-h1" : ""
-        }`}
-      >
-        Top Crypto
-      </h1>
-      <p
-        className={`text-lg mb-8 transition-p ${isVisible ? "visible-p" : ""}`}
-      >
-        Stay updated with the latest and greatest in the world of
-        cryptocurrency.
+    <div className="flex flex-col items-center justify-center h-[50vh] text-white p-8">
+      <h1 className="text-4xl font-bold mb-6">Top Cryptocurrencies</h1>
+      <p className="text-xl mb-6 opacity-80">
+        Stay updated with the latest trends in the world of cryptocurrency.
       </p>
       <button
-        className="px-6 py-3 rounded-full text-white font-semibold"
-        style={{
-          background: "linear-gradient(90deg, #542484, #1e0771, #902174)",
-        }}
+        className="px-8 py-3 rounded-full font-semibold text-white bg-gradient-to-r from-[#542484] to-[#902174] shadow-lg transform transition-transform duration-300 hover:scale-105"
         onClick={() => nav("/top10-crypto")}
       >
         Explore Now
