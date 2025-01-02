14f380f668b9a6372bf03cc8c867d6b87eb19789
adityaexe <kmradi2002@gmail.com>
2025-01-02 08:58:15 +0530

    refactor WelcomeMessage component to improve readability and remove unused assets

--- src/Component/WelcomeMessage/WelcomeMessage.jsx
+++ src/Component/WelcomeMessage/WelcomeMessage.jsx
@@ -1,12 +1,6 @@
-import "./welcomeMessage.css";
-import goblin from "./WelcomeMessage-assets/goblin.png";
-import crypto from "./WelcomeMessage-assets/crypto.png";
-
 const WelcomeMessage = () => {
   return (
     <div className="welcome-container z-0 flex items-center justify-center h-auto text-center pt-20 relative">
-      <img src={crypto} alt="Crypto" className="absolute img-1" />
-      <img src={goblin} alt="Goblin" className="absolute img-2" />
       <div className="bg-white bg-opacity-20 p-8 rounded-lg shadow-lg max-w-xl mx-auto relative z-10">
         <h1 className="text-4xl font-bold text-white mb-4">
           Welcome to CryptoWorld!
