23870ad81da41f3152057fa9121f1fde1773f235
adityaexe <kmradi2002@gmail.com>
2025-01-02 08:58:04 +0530

    refactor Hero component for improved layout and styling

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -1,26 +1,36 @@
-import "./Hero.css"; // Import the custom CSS file
+import React from "react";
 import bitcoin from "./Hero-assets/bitcoin.png";
 import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <section className="hero-section z-0 text-white overflow-hidden">
-      <div className="absolute inset-0 flex items-center">
-        <img
-          src={bitcoin}
-          alt="Crypto Background"
-          className="hero-image rotate"
-        />
-      </div>
-      <div className="absolute bottom-0 right-0 p-8 md:p-16 text-right">
-        <h1 className="hero-title">Secure Your Future with Crypto</h1>
-        <p className="hero-subtitle">
-          Join the digital revolution and invest in the most promising
-          cryptocurrencies today.
-        </p>
-        <Link to={"/all"} className="hero-button">
-          Get Started
-        </Link>
+    <section className="relative  text-blue-950 py-20 h-screen flex items-center justify-center">
+      <div className="flex items-center justify-center space-x-8">
+        {/* Image Section */}
+        <div className="w-1/2">
+          <img
+            src={bitcoin}
+            alt="Bitcoin"
+            className="w-64 h-64 object-contain mx-auto"
+          />
+        </div>
+
+        {/* Text Section */}
+        <div className="w-1/2 text-center md:text-left">
+          <h1 className="text-4xl md:text-5xl font-bold mb-4">
+            The Future of Cryptocurrency is Here
+          </h1>
+          <p className="text-lg mb-6">
+            Unlock the power of digital currencies with our fast, secure, and
+            user-friendly platform.
+          </p>
+          <Link
+            to={"/all"}
+            className="inline-block px-8 py-3 text-lg font-semibold text-black bg-gray-800 rounded-lg shadow-lg hover:bg-gray-600 transition duration-300"
+          >
+            Get Started
+          </Link>
+        </div>
       </div>
     </section>
   );
