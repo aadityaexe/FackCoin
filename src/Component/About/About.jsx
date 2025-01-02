6ff5ee94a9e591ccffd205b5e1830f0654da9440
adityaexe <kmradi2002@gmail.com>
2025-01-02 09:22:52 +0530

    refactor About component for improved text contrast and styling

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -1,11 +1,11 @@
 const About = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center py-12 px-6 ">
-      <div className="max-w-4xl mx-auto shadow-xl rounded-lg p-10 bg-gray-900 bg-opacity-80 backdrop-blur-md">
-        <h1 className="text-4xl font-extrabold text-center text-white mb-8">
+      <div className="max-w-4xl mx-auto shadow-xl rounded-lg p-10  bg-opacity-80 backdrop-blur-md">
+        <h1 className="text-4xl font-extrabold bg-[#1b2522] text-cyan-700 text-center rounded-xl text-centermb-8">
           About CryptoSite
         </h1>
-        <p className="text-gray-300 leading-relaxed mb-6">
+        <p className=" leading-relaxed mb-6 text-black rounded-xl">
           Welcome to{" "}
           <span className="text-indigo-400 font-medium">CryptoSite</span>, your
           trusted source for the latest updates and insights into the dynamic
@@ -13,18 +13,18 @@ const About = () => {
           making cryptocurrency knowledge accessible, actionable, and easy to
           understand.
         </p>
-        <p className="text-gray-300 leading-relaxed mb-6">
+        <p className=" leading-relaxed mb-6  text-black rounded-xl">
           Our dedicated team of experts, with years of experience in finance,
           technology, and blockchain, is committed to delivering accurate and
           timely information. We strive to equip you with the knowledge needed
           to make well-informed decisions in this ever-evolving industry.
         </p>
-        <p className="text-gray-300 leading-relaxed mb-6">
+        <p className=" leading-relaxed mb-6  text-black rounded-xl">
           Whether you are an experienced investor or new to the cryptocurrency
           space, CryptoSite offers resources, tools, and guidance to help you
           navigate and thrive in the market.
         </p>
-        <p className="text-gray-300 leading-relaxed">
+        <p className="leading-relaxed  text-black rounded-xl">
           Thank you for choosing CryptoSite as your go-to platform for
           cryptocurrency insights. Together, we aim to unlock the potential of
           blockchain and digital assets.
