3d08cbfcc3982338dced5437556aa308fcf38727
adityaexe <kmradi2002@gmail.com>
2025-01-02 09:09:41 +0530

    refactor Hero component for improved layout and responsive design

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <section className="relative  text-blue-950 py-20 h-screen flex items-center justify-center">
-      <div className="flex items-center justify-center space-x-8">
+    <section className="relative text-blue-950 py-20 h-screen flex items-center justify-center">
+      <div className="flex flex-col-reverse md:flex-row items-center justify-center space-x-8">
         {/* Image Section */}
-        <div className="w-1/2">
+        <div className="w-1/2 hidden md:block">
           <img
             src={bitcoin}
             alt="Bitcoin"
@@ -17,16 +17,16 @@ const Hero = () => {
 
         {/* Text Section */}
         <div className="w-1/2 text-center md:text-left">
-          <h1 className="text-4xl md:text-5xl font-bold mb-4">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl p-4 sm:p-6 font-bold text-white rounded-xl bg-[#1b2522] mb-4">
             The Future of Cryptocurrency is Here
           </h1>
-          <p className="text-lg mb-6">
+          <p className="text-base sm:text-lg md:text-xl mb-6 rounded-xl text-black">
             Unlock the power of digital currencies with our fast, secure, and
             user-friendly platform.
           </p>
           <Link
             to={"/all"}
-            className="inline-block px-8 py-3 text-lg font-semibold text-black bg-gray-800 rounded-lg shadow-lg hover:bg-gray-600 transition duration-300"
+            className="inline-block px-6 sm:px-8 py-3 text-sm sm:text-lg font-semibold text-white bg-gray-800 rounded-lg shadow-lg hover:bg-gray-600 transition duration-300"
           >
             Get Started
           </Link>
