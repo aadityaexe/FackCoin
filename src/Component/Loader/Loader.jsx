740784e65dce65d4d782bcd3c08257a7af71ff4d
adityaexe <kmradi2002@gmail.com>
2025-01-03 14:12:23 +0530

    add Loader component with custom styling and animation

---
+++ src/Component/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import "./Loader.css"; // Create a CSS file for the styles
+
+const Loader = () => {
+  return (
+    <>
+      <div className="flex justify-center h-[100vh] items-center text-center">
+        <div className="loader  "></div>
+      </div>
+    </>
+  );
+};
+
+export default Loader;
