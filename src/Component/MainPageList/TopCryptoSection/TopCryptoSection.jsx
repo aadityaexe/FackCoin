a28f8dbcaf82fc560c1ba0a36362cafc8a88b3e3
adityaexe <kmradi2002@gmail.com>
2025-01-03 20:37:26 +0530

    refactor TopCryptoSection button styling for improved visibility

--- src/Component/MainPageList/TopCryptoSection/TopCryptoSection.jsx
+++ src/Component/MainPageList/TopCryptoSection/TopCryptoSection.jsx
@@ -12,7 +12,7 @@ const TopCryptoSection = () => {
         Stay updated with the latest trends in the world of cryptocurrency.
       </p>
       <button
-        className="px-8 py-3 rounded-full font-semibold text-white bg-gradient-to-r from-[#542484] to-[#902174] shadow-lg transform transition-transform duration-300 hover:scale-105"
+        className="px-8 py-3 rounded-full font-semibold text-white bg-[#1b2522] shadow-lg transform transition-transform duration-300 hover:scale-105"
         onClick={() => nav("/top10-crypto")}
       >
         Explore Now
