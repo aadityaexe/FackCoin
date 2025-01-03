2362f8cab8d5498485ed017287784e4b93aa011c
adityaexe <kmradi2002@gmail.com>
2025-01-03 20:36:24 +0530

    refactor Services component for improved header styling and background

--- src/Component/Services/Services.jsx
+++ src/Component/Services/Services.jsx
@@ -28,7 +28,7 @@ const Services = () => {
   return (
     <section className="py-12">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-purple-700 text-center mb-8">
+        <h2 className="text-3xl font-bold text-white rounded-xl bg-[#1b2522] p-4 text-center mb-8">
           Our Services
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
